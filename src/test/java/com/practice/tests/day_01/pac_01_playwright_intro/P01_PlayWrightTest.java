28ef8d8abec19af854c8794e1ee48478e5444065
Abbos Shirinboev <58409711+AbbosB12@users.noreply.github.com>
2024-04-30 22:47:41 -0400

    init commit

---
+++ src/test/java/com/practice/tests/day_01/pac_01_playwright_intro/P01_PlayWrightTest.java
@@ -0,0 +1,20 @@
+package com.practice.tests.day_01.pac_01_playwright_intro;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class P01_PlayWrightTest {
+    public static void main(String[] args) throws InterruptedException {
+        Playwright playwright = Playwright.create();
+        BrowserType chromium = playwright.chromium();
+        Browser browser = chromium.launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+        page.navigate("https://google.com");
+        Thread.sleep(3000);
+        page.close();
+        browser.close();
+        playwright.close();
+    }
+}
