3558341132e086bea509862982bce44b7bceade6
Abbos Shirinboev <58409711+AbbosB12@users.noreply.github.com>
2024-05-23 20:21:57 -0400

    Locators and getText

--- src/test/java/com/practice/tests/day_01/pac_02_locators_getText_getAttribute/P02_ClickLinkWithText.java
+++ src/test/java/com/practice/tests/day_01/pac_02_locators_getText_getAttribute/P02_ClickLinkWithText.java
@@ -4,29 +4,26 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class P01_GetTitleGetUrl {
+public class P02_ClickLinkWithText {
     @Test
-    void test1() {
+    void test() {
         Playwright playwright = Playwright.create();
         BrowserType chromium = playwright.chromium();
         Browser browser = chromium.launch(new BrowserType.LaunchOptions().setHeadless(false));
         Page page = browser.newPage();
         page.navigate("https://practice.cydeo.com/");
-        System.out.println(page.url());
-        assertTrue(page.url().contains("cydeo"));
-        System.out.println(page.title());
-        assertEquals("Practice",page.title());
+//        page.setViewportSize(1920,1080);
         page.waitForTimeout(3000);
+        page.click("text=A/B Testing");
+        page.waitForTimeout(3000);
+        System.out.println(page.title());
         page.close();
         browser.close();
         playwright.close();
     }
-
-
 }
